4ea2780c03e0b051beef524e5f870bd2481f6ff4
HAD141 <132294789+HAD141@users.noreply.github.com>
2023-05-06 12:29:34 -0300

    Modificación del id

--- obtencion_datos_raw.py
+++ obtencion_datos_raw.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # url de donde voy a descargar el dataset
-url = 'https://drive.google.com/uc?id=1KhPl1gaejgxmm5GNTRu3fVqKjIkLlA91ql5fZMT1wjs'
+url = 'https://drive.google.com/uc?id=1-eyAJLxk-pQQHO9sRkQ_BFGZAuzFtPSw'
 # levanto el dataset
 data = pd.read_csv(url)
 # verificamos si existe el directorio "dataset"
